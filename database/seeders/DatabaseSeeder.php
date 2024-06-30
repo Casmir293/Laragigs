e2f10c25d2594522e51fe49b2371990bb1f04d9e
Casmir Onyeka <casmir293@gmail.com>
2024-06-30 23:38:37 +0100

    feat: initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(5)->create();
+        Listing::factory(6)->create();
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'acme@test.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iusto consectetur eligendi similique omnis, culpa dignissimos excepturi, quasi, sapiente nobis hic voluptate? Veniam temporibus officiis, nemo ad voluptatum dolore blanditiis?',
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Vuejs Junior Developer',
+        //     'tags' => 'vuejs, javascript',
+        //     'company' => 'Tripple Pan',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'tripple@test.com',
+        //     'website' => 'https://www.tripple.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iusto consectetur eligendi similique omnis, culpa dignissimos excepturi, quasi, sapiente nobis hic voluptate? Veniam temporibus officiis, nemo ad voluptatum dolore blanditiis?',
+        // ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+
+// All Listing
+Route::get('/', function () {
+    return view(
+        'listings',
+        [
+            'heading' => 'Latest Listings',
+            'listings' => Listing::all()
+        ]
+    );
+});
+
+// Single Listing
+Route::get('/listings/{id}', function ($id) {
+    return view(
+        'listing',
+        [
+            'listing' => Listing::find($id)
+        ]
+    );
+});
