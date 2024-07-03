0d047340428bc3b061b539f33c2ae68e3ff0571d
Casmir Onyeka <casmir293@gmail.com>
2024-07-03 23:19:32 +0100

    feat: modified the frontend and  backend code structure.

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,20 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
 
+// COMMON RESOURCE ROUTES:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing  
+
 // All Listing
-Route::get('/', function () {
-    return view(
-        'listings',
-        [
-            'heading' => 'Latest Listings',
-            'listings' => Listing::all()
-        ]
-    );
-});
+Route::get('/', [ListingController::class, 'index']);
 
 // Single Listing
-Route::get('/listings/{listing}', function (Listing $listing) {
-    return view(
-        'listing',
-        [
-            'listing' => $listing
-        ]
-    );
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
